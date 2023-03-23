50c6dd0ab598bd0be996c49f9e958089975574cb
KG <KeilGriesLP@gmail.com>
2023-03-23 11:25:01 -0400

    Added line generation to notebook pages.

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from fpdf import FPDF
 import pandas as pd
 
+# Create and format the PDF object
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 pdf.set_auto_page_break(auto=False, margin=0)
 
+# Create dataframe to read .csv file
 df = pd.read_csv('topics.csv')
 
+# Iterate over csv data to create/format the main topic page
 for index, row in df.iterrows():
     pdf.add_page()
 
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
-    pdf.line(10, 21, 200, 21)
+
+
+    # Add lines to the page
+    for y in range(21, 291, 10):
+        pdf.line(10, y, 200, y)
+
+#     Alternate 'Add Lines' method
+#   ll = 21
+#   while ll < 291:
+#       pdf.line(10, ll, 200, ll)
+#       ll = ll + 10
 
     # Set the footer
     pdf.ln(265)
@@ -20,9 +33,13 @@ for index, row in df.iterrows():
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
-
+    # Add and format "extra" pages
     for i in range(row['Pages'] - 1):
         pdf.add_page()
+
+        # Add lines to the page
+        for y in range(21, 291, 10):
+            pdf.line(10, y, 200, y)
 
         # Set the footer
         pdf.ln(277)
